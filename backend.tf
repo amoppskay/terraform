a8dc84c0798cb3bee07c411bbad66d21e3e5b3b8
amoppskay <99033838+amoppskay@users.noreply.github.com>
2022-08-04 20:07:36 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
- terraform {
-   backend "s3" {
-    bucket = "amps-state-files"
-    key    = "amopps-state-file"
-    region = "us-east-1"
-    profile = "default"
-dynamodb_table= "project-module"
-   }
- }
+#   terraform {
+#    backend "s3" {
+#     bucket = "amps-state-files"
+#     key    = "amopps-state-file"
+#     region = "us-east-1"
+#     profile = "default"
+# dynamodb_table= "project-module"
+#    }
+#  }
 
